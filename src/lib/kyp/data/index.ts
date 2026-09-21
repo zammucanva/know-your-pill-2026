9f0bada6d3d6bf94c2ce8627be32ffa1a7cf7e63
psychocoder <zamaanalishamji@gmail.com>
2026-09-22 04:44:23 +0530

    fix: remove stray content registry export from locked client barrel

--- src/lib/kyp/data/index.ts
+++ src/lib/kyp/data/index.ts
@@ -21,4 +21,3 @@ export * from "./diseases/index";
 export * from "./disease-types";
 export * from "./substances/index";
 export * from "./substance-types";
-\nexport * from "./content-registry";\n
